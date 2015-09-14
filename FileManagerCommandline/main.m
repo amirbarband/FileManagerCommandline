98541cd2ae7530ddace402a5a3710d98cf9cde07
Amir Barband <apple@Amir-2.local>
2015-09-14 19:50:21 +0430

    Initial Commit

---
+++ FileManagerCommandline/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FileManagerCommandline
+//
+//  Created by Amir Barband on 9/14/15.
+//  Copyright (c) 2015 Amir Barband. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
